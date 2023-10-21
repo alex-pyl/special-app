282c09398ceb609139d216449e168ecec3510a0c
Pylaev Alexey <ayboleet@yandex.ru>
2023-10-21 17:18:14 +0300

    some changes

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int value = new Random().nextInt(100);
         System.out.println("Угадайте число 0-100");
-        /*while(true){
+        while(true){
             int attempt = new Scanner(System.in).nextInt();
             if(value == attempt){
                 System.out.println("Вы угадали!");
@@ -14,13 +14,6 @@ public class Main {
             else{
                 System.out.println(value > attempt ? "Загаданное число больше" : "Загаданное число меньше");
             }
-        }*/
-        for(int i=0;i<10;){
-            if(i<5){
-                i++;
-            }
-            i++;
-            System.out.println("YES");
         }
     }
 }
